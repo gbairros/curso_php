d9bf17806d189ed1529e487c16bae507cb3f32db
gbairros <gbairros@gmail.com>
2022-06-20 16:40:35 -0300

    upload

---
+++ examples/session/carrinho.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrinho</title>
+</head>
+<body>
+<form action="add-to-cart.php" method="post">
+   <label for="">Name</label>
+   <input type="text" name="name">
+   <br> <br>
+
+   <label for="">Money</label>
+   <input type="text" name="money">
+   <br> <br>
+
+   <input type="submit" value="Submit">
+</form>
+</body>
+</html>
