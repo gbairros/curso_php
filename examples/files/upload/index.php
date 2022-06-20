eb01727e2b6ebe40f714c03833b121dc56ca0b0b
Gildo <gbairros@gmail.com>
2022-06-20 16:51:41 -0300

    alteracao

--- examples/files/upload/index.php
+++ examples/files/upload/index.php
@@ -5,7 +5,7 @@
   <body>
     <form action="upload.php" method="post" enctype="multipart/form-data">
       Selecione o arquivo: <input type="file" name="arquivo" />
-      <input type="submit" value="Enviar"/>
+      <input type="submit" value="Enviar Arquivo"/>
     </form>
   </body>
 </html>
