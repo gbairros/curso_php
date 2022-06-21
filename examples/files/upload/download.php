a5f82ffc43bb90a58cd0f8a27374127e233112a0
gbairros <gbairros@gmail.com>
2022-06-21 20:50:41 -0300

    download image

---
+++ examples/files/upload/download.php
@@ -0,0 +1,26 @@
+<?php 
+
+$con = new PDO('mysql:host=localhost;dbname=uploaddb', "root", "serpro");
+$sql = "SELECT * FROM avatars";
+$stmt = $con->prepare($sql);
+$rs = $stmt->execute();
+$dados = $stmt->fetchAll();
+
+?>
+
+<html>
+<body>
+	<h2>Imagens</h2>
+	<?php
+		foreach($dados as $item){
+			echo $item['id']." - ";
+			echo $item['email']." - ";
+			//echo "<a href=\"$item['photo_path']\"> Foto </a>";
+			echo "<a href=\"".$item['photo_path']."\"> Foto </a><br>";
+				
+				
+		}
+	?>
+	
+</body>
+</html>
